217ddf539ab13626b25f2ad35e69bc43fd5e1124
Tan-vai <135997970+Tan-vai@users.noreply.github.com>
2023-11-15 13:42:44 +0600

    Add files via upload

---
+++ TR.py
@@ -0,0 +1,11 @@
+import os,requests,sys
+
+print('\033[38;5;46m<\x1b[38;5;208m/\033[38;5;46m> ASSALAMUALAIKUM <\x1b[38;5;208m/\033[38;5;46m>')
+print('\033[38;5;46m<\x1b[38;5;208m/\033[38;5;46m>Follow me Facebook')
+os.system('xdg-open https://facebook.com/abutanim.chowdury') 
+
+os.system('git pull')
+
+import TRM
+
+TRM.login()
