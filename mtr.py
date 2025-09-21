31f8eeda319e535ec7ba8eca4b21e98b90b30b5f
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-09-22 01:28:59 +0600

    Update mtr.py

--- mtr.py
+++ mtr.py
@@ -1,6 +1,9 @@
 import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
+os.system('clear')
+print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Follow My Github')
+os.system('xdg-open https://github.com/mrtan-official')
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 64 Bit user')
